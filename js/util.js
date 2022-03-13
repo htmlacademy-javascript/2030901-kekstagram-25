65056cf0ec86f548138bbc9874249ce20a5b0492
Ovchinnikova Adelina Aleksandrovna <Ovchinnikova@ibs.ru>
2022-03-13 22:09:46 +0500

    Сделала отображение фотографий на странице

--- js/util.js
+++ js/util.js
@@ -1,10 +1,4 @@
-/**
- * Возвращает случайное число диаппазона (включительно)
- * @param {number} min - целое число >=0
- * @param {number} max - целое число >=0
- * @returns {number} - возвращает случайное число. Если min или max отрицательные числа, то возвращает -1
- */
-function getRandomInt(min, max){//Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max){
   if(min<0||max<0) {
     return -1;
   }
@@ -13,12 +7,6 @@ function getRandomInt(min, max){//Источник: https://developer.mozilla.or
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/**
- * Проверяет соответствие проверяемой длины строки с максимально допустимой длиной
- * @param {string} checkString - проверяемая строка
- * @param {number} maxLength - максимально допустимая длина строки
- * @returns {boolean} - возвращает true или false
- */
 function checkLengthString(checkString,maxLength){
   return checkString.length <= maxLength;
 }
