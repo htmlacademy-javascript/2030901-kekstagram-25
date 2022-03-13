c0c4d6bea670c5e558ce7150619d9ef3e1ad9914
Keks <keks@htmlacademy.ru>
2022-03-13 20:12:00 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,5 @@
+import {createdPhoto} from './data.js';
+import {showPhotos} from './showPhotos.js';
+
+const allPhotos = createdPhoto();
+showPhotos(allPhotos);
---
+++ 5/js/util.js
@@ -0,0 +1,16 @@
+function getRandomInt(min, max){
+  if(min<0||max<0) {
+    return -1;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkLengthString(checkString,maxLength){
+  return checkString.length <= maxLength;
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+export {getRandomInt, checkLengthString, getRandomArrayElement};
