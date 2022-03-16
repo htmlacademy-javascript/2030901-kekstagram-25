442e48439c627b9ec173dba821942fdc55608714
Ovchinnikova Adelina Aleksandrovna <Ovchinnikova@ibs.ru>
2022-03-17 00:00:28 +0500

    Добавила функции для открытия фото-карточки с комментариями

--- js/showPhotos.js
+++ js/showPhotos.js
@@ -3,11 +3,12 @@ const photoTemplate = document.querySelector('#picture').content;
 const miniPhotos = document.createDocumentFragment();
 
 function showPhotos(photos) {
-  photos.forEach(({ url, likes, comments }) => {
+  photos.forEach(({ url, likes, comments }, index) => {
     const photo = photoTemplate.cloneNode(true);
     photo.querySelector('.picture__img').src = url;
     photo.querySelector('.picture__likes').textContent = likes;
     photo.querySelector('.picture__comments').textContent = comments.length;
+    photo.querySelector('.picture').dataset.cardId = index;
     miniPhotos.append(photo);
   });
   usersListPhotos.append(miniPhotos);
