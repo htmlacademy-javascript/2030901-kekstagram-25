75bb9c32860bd5e41edd30413522e5720beeee10
Ovchinnikova Adelina Aleksandrovna <Ovchinnikova@ibs.ru>
2022-04-09 22:12:11 +0500

    добавила валидацию при добавлении фото

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import {createdPhoto} from './data.js';
 import {showPhotos} from './showPhotos.js';
 import {createDataForBigPhotos} from './openDataPhoto.js';
+import {checkEscapeDown, photoFormValidation} from './formValidation.js';
 
 const allPhotos = createdPhoto();
 showPhotos(allPhotos);
 createDataForBigPhotos(allPhotos);
+checkEscapeDown();
+photoFormValidation();
